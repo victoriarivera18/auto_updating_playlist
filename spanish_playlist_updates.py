f041abef8b5de705c4848d0de26bae11731c5b10
victoriarivera18 <victoriaeugenia2018@gmail.com>
2022-06-06 11:01:08 -0500

    first commit

---
+++ spanish_playlist_updates.py
@@ -0,0 +1,15 @@
+#1: Access user playlists and saved (liked) songs
+#2: Get list of all the tracks (by name or ID) in the saved songs
+#3: For every song
+#       - check language
+#        - if spanish: 
+#                - check if is already in spanish playlist
+#                       - if no: add to playlist
+
+#                       -else: skip
+
+#        - else: skip do nothing
+#       
+#
+
+#4: improvement: only check the latest 20-25 songs added 
