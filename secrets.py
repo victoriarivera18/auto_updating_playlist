984a49f462ec80a8a6b6777e05c9e1a3d92a7d90
victoriarivera18 <victoriaeugenia2018@gmail.com>
2022-06-13 21:43:33 -0500

    Setting up auth services

---
+++ secrets.py
@@ -0,0 +1,5 @@
+user_uri = "spotify:user:zw1ebo203lsyfiqlm5htu54gl"
+spotify_user_id = user_uri[13 : ]
+
+client_id =  "b67739ca3fa24bb5b009b36ee9259c7f"
+client_secret = "--------------------------------"
--- spanish_playlist_updates.py
+++ spanish_playlist_updates.py
@@ -13,3 +13,65 @@
 #
 
 #4: improvement: only check the latest 20-25 songs added 
+
+import json
+import urllib3
+import base64
+import requests
+import sys
+
+from secrets import spotify_user_id, client_id, client_secret
+
+
+
+def auth(): 
+    client_cred_b64 = base64.b64encode(bytes(client_id + ":" + client_secret, "ISO-8859-1")).decode("ascii")
+    method = "POST"
+
+    url = "https://accounts.spotify.com/api/token"
+    
+    data = {
+        "grant_type" : "client_credentials"
+    }
+
+    headers = {
+        "Authorization" : f"Basic {client_cred_b64}",
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+
+    token_response = requests.post(url, data=data, headers=headers)
+    token_data = token_response.json()
+
+
+
+
+if __name__ == "__main__":
+    #start program
+    auth()
+
+    #set variables
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# query = "https://api.spotify.com/v1/users/{}/playlists".format(spotify_user_id) # using secrets information
+#   response = requests.get(query, data=None, headers={ # from api documentation
+#       "Accept": "application/json",
+#       "Content-Type": "application/json",
+#       "Authorization": "Bearer {}".format(spotify_token)
+#   })
+
+#  response_json = response.json() #playlists name info
